2934348eb9600ffecb6f832a4a7c6108413da32e
Lavish <lalitchoudhary112000@gmail.com>
2025-12-23 18:35:21 +0530

    Update robots.txt to block admin routes

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/admin/'],
+        disallow: ['/api/', '/admin/', '/dashboard/'],
       },
     ],
     sitemap: `${SITE_CONFIG.url}/sitemap.xml`,
